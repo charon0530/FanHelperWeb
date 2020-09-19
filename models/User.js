de834b2ae7f5323c621dbcb064e5cdb2a24d2aef
charon0530 <45917075+charon0530@users.noreply.github.com>
2020-09-19 13:44:20 +0900

    Bcrypt를 이용한 암호화

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 //사용자 모델을 만드는 곳
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -29,6 +31,27 @@ const userSchema = mongoose.Schema({
     toeknExp: {
         type: Number
     }
+})
+//user모델의 save 함수 이전에 행하는 행위 정의(암호화 등)
+userSchema.pre('save',function(next){
+    var user = this // 현재 이 함수를 진행하고있는 인스턴스
+
+    //비밀번호가 바뀔때만 암호화하면됨
+    if (user.isModified ('password')) {
+        //비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }
+
+    
+
 })
 
 const User = mongoose.model("User",userSchema)
